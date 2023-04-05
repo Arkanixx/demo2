a47ef0d4280abb65f4cfa39636994394108f2754
Your Name <you@example.com>
2023-04-05 17:16:08 -0400

    :
    asd

---
+++ Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    private String color;
+
+    Car(){
+        setColor("Red");
+    }
+
+    public void setColor(String c) {
+        this.color =c;
+    }
+    public String getColor(){
+        return color;
+    }
+
+
+}
